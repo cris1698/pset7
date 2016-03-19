82823aa4132391ab0b4716837f4635d77808c561
cristian salas <cristianenrique1698@gmail.com>
2016-03-19 20:06:50 +0000

    prices

--- views/quote.php
+++ views/quote.php
@@ -1,6 +1,6 @@
 <body style="background-color:#BCE8F9">
 <h1><?= $stock["name"] ?></h1>
-<p> Price:$ <?= $stock["price"]?></p>
+<p style ="color: #CE9B11"> Price:$ <?= $stock["price"]?></p>
 <div>
 <?php $stock = lookup($_POST["symbol"]); ?>
 <a class="btn btn-default" href="buy.php?symbol=<?= $stock["symbol"] ?>" role="button" style ="color: #2BCB8B">
